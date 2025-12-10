b69a030b609acfc73bc917c996866ab3b5af97ee
Guadalupe Rincón <rincongp21@gmail.com>
2025-12-09 19:49:59 -0600

    first-commit
    first-commit

---
+++ BussinessLayer/Interfaces/INotaServicio.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Interfaces
+{
+    public interface INotaServicio
+    {
+        Task<List<Nota>> GetAll();
+        Task<Nota?> GetById(int id);
+        Task Create(Nota nota);
+        Task Update(Nota nota);
+        Task Delete(int id);
+    }
+}
---
+++ NotaApp/Program.cs
@@ -0,0 +1,40 @@
+using BussinessLayer.Interfaces;
+using BussinessLayer.Servicios;
+using DataAccessLayer.Datos;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppNoteDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<INotaServicio, NotaServicio>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Nota}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
